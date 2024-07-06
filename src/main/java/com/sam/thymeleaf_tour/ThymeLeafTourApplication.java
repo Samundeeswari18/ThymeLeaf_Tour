55bb1254596e6a8db463c68da896fd4ab9b14431
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 01:39:12 -0400

    ThymeLeaf tour

---
+++ src/main/java/com/sam/thymeleaf_tour/ThymeLeafTourApplication.java
@@ -0,0 +1,13 @@
+package com.sam.thymeleaf_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeLeafTourApplication.class, args);
+    }
+
+}
